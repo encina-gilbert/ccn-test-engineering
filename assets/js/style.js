bf7ae20d8d36d42ded6336544669dc8adcea94c6
Gilbert Encina <encina.gilbert@gmail.com>
2020-03-06 21:07:33 +0800

    modified navigation bar

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,18 +1,18 @@
-$(window).ready(function() {
-  if ($(window).scrollTop() === 0) {
-    $('nav .brand-logo .logo').css({ height: '100px' });
-  }
-});
-$(window).scroll(function() {
-  if ($(window).scrollTop()) {
-    $('nav .brand-logo .logo').stop();
-    if (window.innerWidth < 600) {
-      $('nav .brand-logo .logo').animate({ height: '57px' }, 'fast');
-    } else {
-      $('nav .brand-logo .logo').animate({ height: '64px' }, 'fast');
-    }
-  } else {
-    $('nav .brand-logo .logo').stop();
-    $('nav .brand-logo .logo').animate({ height: '100px' }, 'fast');
-  }
-});
+// $(window).ready(function() {
+//   if ($(window).scrollTop() === 0) {
+//     $('nav .brand-logo .logo').css({ height: '100px' });
+//   }
+// });
+// $(window).scroll(function() {
+//   if ($(window).scrollTop()) {
+//     $('nav .brand-logo .logo').stop();
+//     if (window.innerWidth < 600) {
+//       $('nav .brand-logo .logo').animate({ height: '57px' }, 'fast');
+//     } else {
+//       $('nav .brand-logo .logo').animate({ height: '64px' }, 'fast');
+//     }
+//   } else {
+//     $('nav .brand-logo .logo').stop();
+//     $('nav .brand-logo .logo').animate({ height: '100px' }, 'fast');
+//   }
+// });
