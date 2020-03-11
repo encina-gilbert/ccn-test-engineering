911d54b4695409a92525da130a26ad45401b78f5
Gilbert Encina <encina.gilbert@gmail.com>
2020-03-12 05:54:27 +0800

    Optimized UI

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,10 +1,10 @@
 $(window).ready(function() {
   $('.owl-carousel').owlCarousel({
     autoplay: true,
+    autoplayHoverPause: true,
     center: true,
     loop: true,
     margin: 10,
-    nav: true,
     responsive: {
       0: {
         items: 1
@@ -17,20 +17,4 @@ $(window).ready(function() {
       }
     }
   });
-  //   if ($(window).scrollTop() === 0) {
-  //     $('nav .brand-logo .logo').css({ height: '100px' });
-  //   }
-  // });
-  // $(window).scroll(function() {
-  //   if ($(window).scrollTop()) {
-  //     $('nav .brand-logo .logo').stop();
-  //     if (window.innerWidth < 600) {
-  //       $('nav .brand-logo .logo').animate({ height: '57px' }, 'fast');
-  //     } else {
-  //       $('nav .brand-logo .logo').animate({ height: '64px' }, 'fast');
-  //     }
-  //   } else {
-  //     $('nav .brand-logo .logo').stop();
-  //     $('nav .brand-logo .logo').animate({ height: '100px' }, 'fast');
-  //   }
 });
